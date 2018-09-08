3a98e317964c15e17959871ae860387ea23f70fd
Tomek Owczarek <violeticus@gmail.com>
2018-09-08 21:59:09 +0200

    Add view, simple css and modify ignorefile

--- src/main/java/pl/sda/awesomemoviesclient/awesomemoviesclient/AwesomeMoviesController.java
+++ src/main/java/pl/sda/awesomemoviesclient/awesomemoviesclient/AwesomeMoviesController.java
@@ -1,4 +1,16 @@
 package pl.sda.awesomemoviesclient.awesomemoviesclient;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
 public class AwesomeMoviesController {
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", "stranger");
+        return "welcome";
+    }
 }
