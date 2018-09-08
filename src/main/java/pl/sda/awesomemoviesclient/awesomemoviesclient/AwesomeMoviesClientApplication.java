da07db957587a74c48efc4d0a4a5a0f237a33a8f
Tomek Owczarek <violeticus@gmail.com>
2018-09-08 21:10:21 +0200

    First init commit from Spring init

---
+++ src/main/java/pl/sda/awesomemoviesclient/awesomemoviesclient/AwesomeMoviesClientApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.awesomemoviesclient.awesomemoviesclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeMoviesClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeMoviesClientApplication.class, args);
+	}
+}
