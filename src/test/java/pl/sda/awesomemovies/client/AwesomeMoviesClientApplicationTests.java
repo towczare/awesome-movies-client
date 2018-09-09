953b7f8fe42b8b76353e42799ae1ee96d6ffd4a7
Tomek Owczarek <violeticus@gmail.com>
2018-09-09 13:24:01 +0200

    Small package refactoring
    mend

---
+++ src/test/java/pl/sda/awesomemovies/client/AwesomeMoviesClientApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sda.awesomemovies.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwesomeMoviesClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
