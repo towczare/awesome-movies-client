3639c574fdcfa6635e20ab9bb3f7626f3228bb03
Krzysztof Songowicz <krzysztof.songowicz@outlook.com>
2018-09-16 13:32:17 +0200

    Movies search and filters - added quick search for movies on main page - added advanced search form by given criteria - implemented search in controllers and services

---
+++ src/main/java/pl/sda/awesomemovies/client/movie/FilterCriteria.java
@@ -0,0 +1,16 @@
+package pl.sda.awesomemovies.client.movie;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+public class FilterCriteria {
+
+    private String name;
+}
--- src/main/java/pl/sda/awesomemovies/client/movie/MovieClientController.java
+++ src/main/java/pl/sda/awesomemovies/client/movie/MovieClientController.java
@@ -3,8 +3,9 @@ package pl.sda.awesomemovies.client.movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class MovieClientController {
@@ -13,6 +14,7 @@ public class MovieClientController {
 
     private static final String MOVIE_DETAILS_VIEW = "movie-details-view";
     private static final String MOVIES_VIEW = "movies-list";
+    private static final String MOVIES_SEARCH = "movie-search";
 
     @Autowired
     public MovieClientController(MovieClientService movieClientService) {
@@ -29,5 +31,25 @@ public class MovieClientController {
     public String showMovieDetails(@PathVariable String id, Model model) {
         model.addAttribute("movie", movieClientService.getMovie(Long.valueOf(id)));
         return MOVIE_DETAILS_VIEW;
+    }
+
+    @PostMapping("/filterMovies")
+    public String filterMovies(@RequestParam String searchName, Model model) {
+        List<Movie> filteredMovies = movieClientService.filterMovies(searchName);
+        model.addAttribute("movies", filteredMovies);
+        return MOVIES_VIEW;
+    }
+
+    @GetMapping("/advancedSearch")
+    public String searchForMoviesForm(Model model) {
+        model.addAttribute("filterCriteria", new FilterCriteria());
+        return MOVIES_SEARCH;
+    }
+
+    @PostMapping("/advancedSearch")
+    public String searchForMovies(@ModelAttribute FilterCriteria filterCriteria, Model model){
+        List<Movie> movies = movieClientService.searchForMovies(filterCriteria);
+        model.addAttribute("movies", movies);
+        return MOVIES_VIEW;
     }
 }
