ec00455e81a903ce9032bd9da900ae041c82e0a8
Krzysztof Songowicz <krzysztof.songowicz@outlook.com>
2018-09-23 13:11:10 +0200

    Added ability to search for movies by category

--- src/main/java/pl/sda/awesomemovies/client/category/Category.java
+++ src/main/java/pl/sda/awesomemovies/client/category/Category.java
@@ -14,4 +14,23 @@ import lombok.Setter;
 public class Category {
     private Integer id;
     private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return name != null ? name.equals(category.name) : category.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
--- src/main/java/pl/sda/awesomemovies/client/movie/MovieClientService.java
+++ src/main/java/pl/sda/awesomemovies/client/movie/MovieClientService.java
@@ -3,19 +3,17 @@ package pl.sda.awesomemovies.client.movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
+import pl.sda.awesomemovies.client.category.Category;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class MovieClientService {
 
     private MovieRestService movieRestService;
-
-    private List<Movie> moviesList = new ArrayList<>();
 
     @Autowired
     public MovieClientService(MovieRestService movieRestService) {
@@ -28,8 +26,7 @@ public class MovieClientService {
                     movieRestService.getEndpointUrl() + "/movies",
                     Movie[].class
             );
-            moviesList = Arrays.asList(movies);
-            return moviesList;
+            return Arrays.asList(movies);
         } catch (RestClientException e) {
             e.printStackTrace();
             return Collections.emptyList();
@@ -37,17 +34,20 @@ public class MovieClientService {
     }
 
     Movie getMovie(Long id) {
-        for (Movie movie : moviesList) {
-            if (movie.getId().equals(id)) {
-                return movie;
-            }
+        try {
+            return movieRestService.provide().getForObject(
+                    movieRestService.getEndpointUrl() + "/movie/" + id,
+                    Movie.class
+            );
+        } catch (RestClientException e) {
+            e.printStackTrace();
+            return new Movie();
         }
-        return null;
     }
 
     List<Movie> filterMovies(String searchName) {
         List<Movie> foundMovies = new ArrayList<>();
-        for (Movie movie : moviesList) {
+        for (Movie movie : getAllMovies()) {
             if (movie.getTitle().toUpperCase().contains(searchName.toUpperCase())) {
                 foundMovies.add(movie);
             }
@@ -56,9 +56,12 @@ public class MovieClientService {
     }
 
     List<Movie> searchForMovies(FilterCriteria filterCriteria) {
-        return moviesList.stream().filter(e ->
-                e.getTitle().toUpperCase().contains(filterCriteria.getName().toUpperCase()))
+        List<Movie> foundMovies = new ArrayList<>();
+        getAllMovies().stream().filter(e ->
+                e.getTitle().toUpperCase().contains(filterCriteria.getName().toUpperCase())
+                        && e.getCategories().contains(new Category(filterCriteria.getCategory())))
                 .distinct()
-                .collect(Collectors.toList());
+                .forEach(foundMovies::add);
+        return foundMovies;
     }
 }
