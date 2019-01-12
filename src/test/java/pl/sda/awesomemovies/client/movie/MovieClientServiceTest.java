531bca704cfcf04c13d18cfbe19f5850fbe6eac1
Kamil Olejniczak <kamil.camilus@gmail.com>
2019-01-12 12:06:39 +0100

    #23 - fixing search engine  not returning any results

---
+++ src/test/java/pl/sda/awesomemovies/client/movie/MovieClientServiceTest.java
@@ -0,0 +1,16 @@
+package pl.sda.awesomemovies.client.movie;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MovieClientServiceTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void shouldFindAMovieWithASpecifiedSearchCriteria() {
+    }
+}
